40ddd012bfa8429a39f39b0b04dea18fd4766bf9
Mark de Wever <koraq@xs4all.nl>
2022-12-11 13:00:48 +0100

    Adds more output.
    Shows task ids and the ids of tasks blocking another tasks.

--- kaban.cpp
+++ kaban.cpp
@@ -1,5 +1,7 @@
 #include <algorithm>
+#include <charconv>
 #include <cstdlib>
+#include <format>
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -7,11 +9,47 @@
 #include <vector>
 
 struct task {
-  std::string id;
+  std::size_t id;
   std::string title;
   std::string status;
-  std::string blocked_by_tasks;
+  std::vector<std::size_t> blocked_by_tasks;
 };
+
+std::size_t parse_id(std::string_view input) {
+  std::size_t result;
+  std::from_chars_result status =
+      std::from_chars(input.data(), input.data() + input.size(), result);
+  if (status.ec != std::errc{})
+    throw 42;
+
+  if (status.ptr != input.data() + input.size())
+    throw 42;
+
+  return result;
+}
+
+std::vector<std::size_t> parse_id_list(std::string_view input) {
+  std::vector<std::size_t> result;
+  if (input.empty())
+    return result;
+  const char *begin = input.data();
+  const char *end = input.data() + input.size();
+
+  while (true) {
+    std::size_t value;
+    std::from_chars_result status = std::from_chars(begin, end, value);
+    if (status.ec != std::errc{})
+      throw 42;
+
+    result.push_back(value);
+    begin = status.ptr;
+    if (begin == end)
+      return result;
+
+    if (*begin++ != ',' || *begin++ != ' ')
+      throw 42;
+  }
+}
 
 std::vector<task> parse(std::ifstream &file) {
   bool parse = false;
@@ -28,13 +66,13 @@ std::vector<task> parse(std::ifstream &file) {
 
       int pos = line.find('=');
       if (line.substr(0, pos) == "id")
-        tasks.back().id = line.substr(pos + 1);
+        tasks.back().id = parse_id(line.substr(pos + 1));
       if (line.substr(0, pos) == "title")
         tasks.back().title = line.substr(pos + 1);
       if (line.substr(0, pos) == "status")
         tasks.back().status = line.substr(pos + 1);
       if (line.substr(0, pos) == "blocked_by_tasks")
-        tasks.back().blocked_by_tasks = line.substr(pos + 1);
+        tasks.back().blocked_by_tasks = parse_id_list(line.substr(pos + 1));
 
     } else if (line == "[task]") {
       parse = true;
@@ -58,9 +96,14 @@ void print(std::string_view title, const std::vector<task> &tasks) {
   if (tasks.empty())
     return;
 
-  std::cout << "[" << title << "]\n";
-  for (const auto &task : tasks)
-    std::cout << "- " << task.title << '\n';
+  std::cout << std::format("[{}]\n", title);
+  for (const auto &task : tasks) {
+    std::cout << std::format("- {:3} {}\n", task.id, task.title);
+
+    if (!task.blocked_by_tasks.empty())
+      for (auto id : task.blocked_by_tasks)
+        std::cout << std::format("      x {:3}\n", id);
+  }
   std::cout << '\n';
 }
 
