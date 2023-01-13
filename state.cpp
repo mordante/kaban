ead48d30e3c8cf77b2471f8335253ecc106cb851
Mark de Wever <koraq@xs4all.nl>
2023-01-13 20:25:05 +0100

    Minor resource management improvement.
    There are fixes in Clang that fix memory ownership in modules.

--- state.cpp
+++ state.cpp
@@ -3,7 +3,6 @@
 import data;
 import stl;
 
-#if 0
 template <std::default_initializable T> class tsingleton {
 public:
   tsingleton() = default;
@@ -13,12 +12,21 @@ public:
   tsingleton &operator=(const tsingleton &) = delete;
   tsingleton &operator=(tsingleton &&) = delete;
 
+#if 0
   [[nodiscard]] std::expected<void, nullptr_t> set(std::unique_ptr<T> &&data) {
     if (!data)
       return std::unexpected(nullptr);
     data_ = std::move(data);
     return {};
   }
+#else
+  void set(std::unique_ptr<T> &&data) {
+    if (!data)
+      throw 42;
+    data_ = std::move(data);
+  }
+
+#endif
   // TODO use deducing this.
   [[nodiscard]] T &get() { return *data_; }
   [[nodiscard]] const T &get() const { return *data_; }
@@ -26,31 +34,6 @@ public:
 private:
   std::unique_ptr<T> data_{std::make_unique<T>()};
 };
-#else
-template <std::default_initializable T> class tsingleton {
-public:
-  tsingleton() = default;
-  ~tsingleton() = default;
-  tsingleton(const tsingleton &) = delete;
-  tsingleton(tsingleton &&) = delete;
-  tsingleton &operator=(const tsingleton &) = delete;
-  tsingleton &operator=(tsingleton &&) = delete;
-
-  void set(T *data) {
-    if (!data)
-      throw 42;
-    data_ = data;
-  }
-  // TODO use deducing this.
-  [[nodiscard]] T &get() { return *const_cast<T *>(data_); }
-  [[nodiscard]] const T &get() const { *const_cast<T *>(data_); }
-
-private:
-  // Note in modules the store was omitted.
-  T *data_{new T};
-};
-
-#endif
 
 static tsingleton<data::tstate> state_singleton;
 
@@ -63,5 +46,6 @@ static tsingleton<data::tstate> state_singleton;
 // void set_state(std::unique_ptr<data::tstate> &&state) {
 //  return state_singleton.set(std::move(state));
 void set_state(void *state) {
-  state_singleton.set(static_cast<data::tstate *>(state));
+  state_singleton.set(
+      std::unique_ptr<data::tstate>{static_cast<data::tstate *>(state)});
 }
