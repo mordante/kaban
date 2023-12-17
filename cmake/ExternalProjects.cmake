5c03e9836a588c3fde8ed7e39e9c4d6fa47f47c9
Mark de Wever <koraq@xs4all.nl>
2023-12-17 15:04:39 +0100

    Switches to the latest FTXUI release.

--- cmake/ExternalProjects.cmake
+++ cmake/ExternalProjects.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
   ftxui
   GIT_REPOSITORY https://github.com/ArthurSonzogni/ftxui
-  GIT_TAG v4.1.1
+  GIT_TAG v5.0.0
 )
 FetchContent_GetProperties(ftxui)
 if(NOT ftxui_POPULATED)
